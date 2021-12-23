3780a0a1f8f81d63d1936302ac5588a8adcd7d3e
Bekir Yalcin <70541124+siberblog@users.noreply.github.com>
2021-12-23 19:18:30 +0000

    Test

--- src/index.js
+++ src/index.js
@@ -27,6 +27,48 @@ const gracefulShutdown = () => {
         .then(() => process.exit()); 
 };
 
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit()); 
+};
+
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
 process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
